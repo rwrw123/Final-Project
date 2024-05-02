17191e56ef7fd5f28f19d9307d78af8d00a02b0d
rwrw123 <113308286+rwrw123@users.noreply.github.com>
2024-05-02 16:21:16 -0400

    register namespace and configure the app

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
-from app import create_app
+from flask import Flask
+from flask_restx import Api
+from pymongo import MongoClient
+from users import ns as users_ns
+from devices import ns as devices_ns
+from measurements import ns as measurements_ns
 
-app = create_app()
+app = Flask(__name__)
+api = Api(app)
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client['health_db']
+
+api.add_namespace(users_ns)
+api.add_namespace(devices_ns)
+api.add_namespace(measurements_ns)
 
 if __name__ == '__main__':
     app.run(debug=True)
