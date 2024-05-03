51b0447b725ff9608109586bdfb94bb2dc7bd456
rwrw123 <113308286+rwrw123@users.noreply.github.com>
2024-05-03 02:37:54 -0400

    modify database URI

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/health_db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app = Flask(__name__, instance_relative_config=True)
+    if test_config is None:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/health_db'
+    else:
+        app.config.update(test_config)
 
     db.init_app(app)
 
     with app.app_context():
-        db.create_all()  # Create sql tables for our data models
+        db.create_all()  # Create sql tables for data models
 
     from .routes import main
     app.register_blueprint(main)
