d852fbea99754124eb9e1aa8d4b3224aafb5f91b
rwrw123 <113308286+rwrw123@users.noreply.github.com>
2024-05-02 23:27:58 -0400

    update admin

--- app/admins.py
+++ app/admins.py
@@ -1,10 +1,7 @@
 from flask import jsonify
 from flask_restx import Namespace, Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client['health_db']
+from database import mongo
 
 ns = Namespace('admin', description='Admin operations')
 
@@ -13,7 +10,7 @@ def admin_required(fn):
     @jwt_required()
     def wrapper(*args, **kwargs):
         user_id = get_jwt_identity()
-        user = db.users.find_one({'_id': user_id})
+        user = mongo.db.users.find_one({'_id': user_id})
         if user and 'admin' in user['roles']:
             return fn(*args, **kwargs)
         else:
